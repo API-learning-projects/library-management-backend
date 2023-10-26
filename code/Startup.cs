9c4762ccbb74e54e61bf10c8158e083cb6e5efb7
Justin Slijkhuis <justin.slijkhuis@hotmail.com>
2023-10-26 18:09:21 +0200

    .NET and Entity Framework start code

---
+++ code/Startup.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using LibraryManagement.Data;
+
+namespace LibraryManagement
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(Configuration.GetConnectionString("PostgreSQL")));
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            // Provides more detailed error messages
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            // Forces HTTPS
+            app.UseHttpsRedirection();
+
+            // Enables routing so the API can handle different URLs and HTTP methods
+            app.UseRouting();
+
+            // Enables authentication and authorization
+            app.UseAuthorization();
+
+            // Maps the endpoints to the controller folder
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
--- code/data/ApplicationDbContext.cs
+++ code/data/ApplicationDbContext.cs
@@ -5,12 +5,12 @@ namespace LibraryManagement.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Book> Books { get; set; }
+        
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
         }
-
-        public DbSet<Account> Accounts { get; set; }
-        public DbSet<Book> Books { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
